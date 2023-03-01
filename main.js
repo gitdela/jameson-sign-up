165490d44b6f63ecd9e09c918bce537dc4e508b6
gitdela <scriptingdela@gmail.com>
2023-03-01 02:17:15 +0000

    Add slideshow with JS

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const loginForm = document.querySelector('#login')
-//     const createAccountForm = document.querySelector('#createAccount');
+// Change the image URLs in this array to your own images
+const imageUrls = [
+  './images/jmsn_ss_1.JPG',
+  './images/jmsn_ss_2.JPG',
+  './images/jmsn_ss_3.JPG',
+  './images/jmsn_ss_4.JPG',
+  './images/jmsn_ss_5.JPG',
+];
 
-//     document.querySelector('#linkCreateAccount').addEventListener('click', e => {
-//         loginForm.classList.add('form-hidden');
-//         createAccountForm.classList.remove('form-hidden');
-//     });
+// Get the pseudo-element
+const slideshow = document.querySelector('.container::after');
 
-//     document.querySelector('#linkLogin').addEventListener('click', e => {
-//         loginForm.classList.add('form-hidden');
-//         createAccountForm.classList.remove('form-hidden');
-//     });
-// });
+// Set the background image of the pseudo-element to the first image
+slideshow.style.backgroundImage = `url(${imageUrls[0]})`;
+
+// Use setInterval to change the background image at regular intervals
+let index = 0;
+setInterval(() => {
+  index = (index + 1) % imageUrls.length;
+  slideshow.style.backgroundImage = `url(${imageUrls[index]})`;
+}, 10000);
